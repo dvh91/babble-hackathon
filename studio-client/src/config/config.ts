daacc72359308c61a2051e84f21fc9737dcd8ebb
Dvir Hazut <dvir.hazut@kaltura.com>
2017-01-03 11:20:31 +0200

    dist update

---
+++ studio-client/src/config/config.ts
@@ -0,0 +1,6 @@
+export const BabbleConfig = {
+  username: 'dvir.hazut@kaltura.com',
+  password: 'Babble$win1',
+  smUserId: '14808',
+  smAuthToken: 'Y2Y0M2NjZjMtNWUyNS00NDYyLWE0YWEtOTUzNTBkOTRhY2Y2'
+};
